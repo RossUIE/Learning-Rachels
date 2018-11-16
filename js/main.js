e3a27329d3ae324d88c376c23c04b0b42c378a84
Ross <r.mcmurray1@hotmail.co.uk>
2018-11-16 10:46:27 +0000

    Adding responsiveness to sidebar icon on smaller devices

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-function openNav() {
-    document.getElementById("sidebarMenu").style.width = "80%";
-}
-
-function closeNav() {
-    document.getElementById("sidebarMenu").style.width = "0";
-}
-
 $(window).scroll(function(){
   $(".down").css("opacity", 1 - $(window).scrollTop() / 250); 
 //250 is fade pixels
