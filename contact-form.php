5d8cbaeec887271997143236afc5842ed210d8cf
Ross <r.mcmurray1@hotmail.co.uk>
2018-11-27 18:51:29 +0000

    Changing email on contact form

--- contact-form.php
+++ contact-form.php
@@ -5,9 +5,9 @@
     $mailFrom = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
-     $mailTo = "r.mcmurray1@hotmail.co.uk";
+    $mailTo = "learningatrachels@hotmail.com";
     $headers = "From: ".$mailFrom;
-    $txt = "You have recieved an e-mail from ".$name. ".\n\n".$message;
+    $txt = "You have recieved an e-mail from ".$name. ".\n\n" . "Phone Number (if given):". $phone . ".\n\n" .$message;
      mail($mailTo, $subject, $txt, $headers);
      header("Location: mailSent.html");
 }
